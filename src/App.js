aa6a2b3b15e61a467ec10d65b041ae140c56884d
wzb <13556128068@163.com>
2017-11-07 18:26:08 +0800

    首页

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import {Router, Route, BrowserRouter, Switch, Redirect} from 'react-router-dom';
 import LoginForm from './Login';
 import Layout from './layout/Main';
+import Home from './page/Home';
 
 class App extends Component {
     render() {
         return (
             <Router history={BrowserRouter}>
                 <Route path="/app/login" component={LoginForm}/>
+                <Route path="/app/home" component={Home}/>
                 <Route path="/app/dashboard" component={Layout}/>
             </Router>
         );
